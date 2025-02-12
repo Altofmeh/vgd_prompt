99b4894a167a306412091eddda1c64029d8bdb01
Altofmeh <Chrism210@stemcollegiate.ca>
2025-02-12 09:12:08 -0700

    Create feb12.cs

---
+++ feb12.cs
@@ -0,0 +1,16 @@
+//This is my code for feb 12
+//We learned about strings
+//did not like how I needed to change the copied code
+Console.WriteLine('b');
+Console.WriteLine("Hello World!");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
